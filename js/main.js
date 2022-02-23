e7ffe791ba29e13446878e6847f634de7ad1f6ca
dbessarab <d.bessarab@gmail.com>
2022-02-23 11:30:47 +0100

    40.1

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let firstNumber = 3.10;
 let secondNumber = 1.10;
 
-function myFunction(firstNumber, secondNumber) {
-  let result = firstNumber + secondNumber;
+function myFunction(x, y) {
+  let result = x + y;
   return result;
 }
